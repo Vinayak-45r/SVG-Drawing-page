6859198a130678c823c989bdb0960bf3878d62ca
Vinayak-45r <vinayak.45r@gmail.com>
2026-02-01 19:55:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let svg = document.getElementById("box");
+let drawing = false;
+let line;
+let points = "";
+
+svg.addEventListener("mousedown", function (e) {
+    drawing = true;
+    points = "";
+
+    line = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "polyline"
+    );
+
+    line.setAttribute("stroke", "black");
+    line.setAttribute("stroke-width", "3");
+    line.setAttribute("fill", "none");
+
+    svg.appendChild(line);
+});
+
+svg.addEventListener("mousemove", function (e) {
+    if (!drawing) return;
+
+    let rect = svg.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+
+    points = points + x + "," + y + " ";
+    line.setAttribute("points", points);
+});
+
+svg.addEventListener("mouseup", function () {
+    drawing = false;
+});
+
+function clearBox() {
+    svg.innerHTML = "";
+}
+
+
+
+
